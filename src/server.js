e957f3c069f21f9e7ffc42db380bc4b71b7aa690
ZTeams <97518038+ZTeams@users.noreply.github.com>
2022-07-13 14:03:55 +0900

    zoom fitst

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname +  "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req,res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+const sockets = []
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser  ");
+    socket.on("close", () => console.log("Disconnect from browser X"));
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(message.toString('utf8'))); 
+        // aSocket 각 브라우저를 aSocket으로 표시하고 메세지를 보낸다.
+        // socket.send(message.toString('utf8'));
+        // console.log(message.toString('utf8'));
+        // 강의에선 socket.send(message) 했지만 버전 up 돼서 위 코드처럼 해야 됨
+    });
+});
+
+server.listen(3000, handleListen);
